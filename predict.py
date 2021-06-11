18f969cdd73213e4d5f43b35693860e97d208af5
Aniket Srivastava <43821251+aniketsriv@users.noreply.github.com>
2021-06-11 20:53:48 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,38 @@
+import torch
+import torchvision.transforms as transforms
+from get_dataloader import get_loader
+from model import CNNtoRNN
+from PIL import Image
+
+
+def predict(model, device, dataset, img):
+    transform = transforms.Compose(
+        [
+            transforms.Resize((299, 299)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+
+    model.eval()
+    test_img = transform(Image.open(img).convert("RGB")).unsqueeze(
+        0
+    )
+    print("OUTPUT:"+" ".join(model.caption_image(test_img.to(device), dataset.vocab)))
+
+
+transform = transforms.Compose(
+        [
+            transforms.Resize((299, 299)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+
+loader, dataset = get_loader(
+    "flickr8k/images/", "flickr8k/captions.txt", transform=transform
+    )
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = CNNtoRNN(embed_size=256, hidden_size=256, vocab_size=len(dataset.vocab), num_layers=1).to(device)
+predict(model=model, device=device, dataset=dataset, img="img1.jpg")
---
+++ train.py
@@ -0,0 +1,78 @@
+import torch
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+# from utils import save_checkpoint, load_checkpoint, print_examples
+from get_dataloader import get_loader
+from model import CNNtoRNN
+
+
+def train():
+    transform = transforms.Compose(
+        [
+            transforms.Resize((356, 356)),
+            transforms.RandomCrop((299, 299)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+
+    train_loader, dataset = get_loader(
+        root_folder="flickr8k/images",
+        annotation_file="flickr8k/captions.txt",
+        transform=transform,
+        num_workers=2,
+    )
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_CNN = False
+
+    # Hyperparameters
+    embed_size = 256
+    hidden_size = 256
+    vocab_size = len(dataset.vocab)
+    num_layers = 1
+    learning_rate = 3e-4
+    num_epochs = 10
+
+    # for tensorboard
+    writer = SummaryWriter("runs/flickr")
+    step = 0
+
+    # initialize model, loss etc
+    model = CNNtoRNN(embed_size, hidden_size, vocab_size, num_layers).to(device)
+    criterion = nn.CrossEntropyLoss(ignore_index=dataset.vocab.stoi["<PAD>"])
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Only finetune the CNN
+    for name, param in model.encoderCNN.inception.named_parameters():
+        if "fc.weight" in name or "fc.bias" in name:
+            param.requires_grad = True
+        else:
+            param.requires_grad = train_CNN
+    model.train()
+
+    for epoch in range(num_epochs):
+        print("Epoch {}".format(epoch))
+        for idx, (imgs, captions) in tqdm(
+            enumerate(train_loader), total=len(train_loader), leave=False
+        ):
+            imgs = imgs.to(device)
+            captions = captions.to(device)
+
+            outputs = model(imgs, captions[:-1])
+            loss = criterion(
+                outputs.reshape(-1, outputs.shape[2]), captions.reshape(-1)
+            )
+
+            writer.add_scalar("Training loss", loss.item(), global_step=step)
+            step += 1
+
+            optimizer.zero_grad()
+            loss.backward(loss)
+            optimizer.step()
+
+
+if __name__ == "__main__":
+    train()
