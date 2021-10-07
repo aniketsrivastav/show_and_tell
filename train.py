18bac01c0d905cef5e802805a0fbc001bdd22b2a
joshishruty99 <63907554+joshishruty99@users.noreply.github.com>
2021-10-07 16:30:25 +0530

    Update train.py

--- train.py
+++ train.py
@@ -27,14 +27,15 @@ def train():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     train_CNN = False
 
+    
     # Hyperparameters
-    embed_size = 256
-    hidden_size = 256
-    vocab_size = len(dataset.vocab)
     num_layers = 1
     learning_rate = 3e-4
     num_epochs = 10
-
+    embed_size = 256
+    hidden_size = 256
+    vocab_size = len(dataset.vocab)
+   
     # for tensorboard
     writer = SummaryWriter("runs/flickr")
     step = 0
@@ -65,7 +66,7 @@ def train():
                 outputs.reshape(-1, outputs.shape[2]), captions.reshape(-1)
             )
 
-            writer.add_scalar("Training loss", loss.item(), global_step=step)
+            writer.add_scalar("Training Loss", loss.item(), global_step=step)
             step += 1
 
             optimizer.zero_grad()
